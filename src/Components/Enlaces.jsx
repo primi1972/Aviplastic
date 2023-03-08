1bb32e279040b0eea42d77eb9fb295390e1e633e
PRIMITIVO VALENZUELA SANDOVAL <114181813+primi1972@users.noreply.github.com>
2023-03-08 12:49:49 -0600

    first commit

---
+++ src/Components/Enlaces.jsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+import { Fragment } from "react";
+
+const CardGeneral = ({ color, imagen, titulo, texto }) => {
+  return (
+    <>
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0.6, duration: 1 }}
+      className="flex flex-col text-center py-5 w-[350px]"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`fill-current ${color} m-auto`}
+        height="60"
+        viewBox="0 96 960 960"
+        width="60"
+      >
+        <path d={`${imagen}`} />
+      </svg>
+      <h3 className="text-3xl font-bold">{titulo}</h3>
+      <p className="text-xl font-extralight">{texto}</p>
+      <a href="#" className="waves-effect waves-light btn">Saber mas...</a>       
+    </motion.div>
+    </>
+  );
+};
+export default CardGeneral;
